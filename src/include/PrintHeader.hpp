7de0b79f53af27aa8d81e559fdb1f31ca5a13885
Daniel <daniel.forde001@gmail.com>
2014-05-07 21:25:29 +0200

    Edited PrintHeader function

--- src/include/PrintHeader.hpp
+++ src/include/PrintHeader.hpp
@@ -11,11 +11,11 @@ void PrintHeader (void)
 	std::cout << "=                                      =\n";
 	std::cout << "========================================\n";
 	std::cout << "=                                      =\n";
-	std::cout << "=               06/05/14               =\n";
+	std::cout << "=               07/05/14               =\n";
 	std::cout << "=                                      =\n";
 	std::cout << "========================================\n";
 	std::cout << "=                                      =\n";
-	std::cout << "=             Daniel Forde             =\n";
+	std::cout << "=                0tacon                =\n";
 	std::cout << "=                                      =\n";
 	std::cout << "========================================\n\n";
 }
